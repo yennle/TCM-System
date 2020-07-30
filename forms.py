ec178433ebae0206c5f2cb69859d6a221bc28af9
=yennle <=phiyen263@gmail.com>
2020-07-30 16:58:38 -0400

    login and register

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=5, max=15)])
+    first_name = StringField('First Name',validators=[DataRequired()])
+    last_name = StringField('Last Name',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired(), Length(min=5, max=15)])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     # def validate_username(self,username):
@@ -16,8 +16,7 @@ class RegistrationForm(FlaskForm):
     #         raise ValidationError('That username is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username',validators=[DataRequired(), Length(min=5, max=15)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
